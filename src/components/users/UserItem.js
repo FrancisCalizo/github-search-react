c6824320c93f51399c837e6067e826622c0383e0
Francis Calizo <francisacalizo@gmail.com>
2019-08-15 21:55:18 -0400

    Start Single User Route

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+const UserItem = ({ user: { login, avatar_url, html_url }, getUser }) => {
   // Destructuring here is same as above
   // const { login, avatar_url, html_url } = props.user;
 
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -3,7 +3,7 @@ import UserItem from './UserItem';
 import Spinner from '../layout/Spinner';
 import PropTypes from 'prop-types';
 
-const Users = ({ users, loading }) => {
+const Users = ({ users, loading, getUser }) => {
   if (loading) {
     return <Spinner />;
   } else {
